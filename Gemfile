b91172e1d19635a853ec4e6828e66a542f20b1e1
Eric Haughee <ehaughee@gmail.com>
2018-10-08 18:41:21 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'steam-api'
+gem 'sinatra'
+gem 'haml'
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'steam-api'
+require 'json'
+require 'sinatra'
+
+get '/' do
+    'Navigate to /<your steam id here>/games to see a list of games'
+end
+
+get '/:steam_id/games' do
+    Steam.apikey = File.read('apikey')
+    response = Steam::Player.owned_games(params['steam_id'], params: { include_appinfo: 1 })
+
+    # response["games"].each do |game|
+    #     puts game["name"]
+    # end
+    @games = response["games"]
+    haml :games
+end
