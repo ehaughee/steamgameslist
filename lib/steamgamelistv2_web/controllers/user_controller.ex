e8b055a08745d1d8314bb1db893c243e0157c0fa
Eric Haughee <ehaughee@gmail.com>
2021-09-08 23:05:33 -0700

    Add friends list to user page

--- lib/steamgamelistv2_web/controllers/user_controller.ex
+++ lib/steamgamelistv2_web/controllers/user_controller.ex
@@ -3,7 +3,13 @@ defmodule Steamgamelistv2Web.UserController do
   @game_image_url_base "http://media.steampowered.com/steamcommunity/public/images/apps/" # {appid}/{hash}.jpg
 
   def index(conn, %{ "user_id" => user_id }) do
-    player_info_response = get_player_info(user_id)
+    player_info_response = get_player_infos([user_id])
+    player_friend_list_response = get_player_friend_list(user_id)
+
+    player_friend_profiles = player_friend_list_response["friendslist"]["friends"]
+    |> Enum.map(&(&1["steamid"]))
+    |> get_player_infos()
+
     # TODO: Sort the games by name
     game_info_response = get_player_games(user_id)
 
@@ -13,7 +19,15 @@ defmodule Steamgamelistv2Web.UserController do
       user_id: String.to_integer(user_id),
       games_info: game_info_response["response"],
       player_info: List.first(player_info_response["response"]["players"]),
+      player_friend_profiles: player_friend_profiles["response"]["players"],
       game_image_url_base: @game_image_url_base
     )
+  end
+
+  @spec pmap(list(), function()) :: list()
+  defp pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
   end
 end
