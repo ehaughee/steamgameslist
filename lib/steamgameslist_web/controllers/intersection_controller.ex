5da78141e0ff6ace562d0e74589634f348be733f
Eric Haughee <ehaughee@gmail.com>
2021-12-04 21:44:33 -0800

    Undo games list sorting for user page

--- lib/steamgameslist_web/controllers/intersection_controller.ex
+++ lib/steamgameslist_web/controllers/intersection_controller.ex
@@ -18,7 +18,7 @@ defmodule SteamgameslistWeb.IntersectionController do
       all_user_profiles = List.flatten(get_all_user_profiles(user_ids_list))
       intersection =
         get_intersection(all_user_games)
-        |> Enum.sort_by(&(&1["name"]))
+        |> Enum.sort(&(&2["playtime_forever"] <= &1["playtime_forever"]))
 
       render(
         conn,
